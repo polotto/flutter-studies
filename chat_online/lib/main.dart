c467014bc977c3a196b3dd06926e98b713937fe3
Ângelo Polotto <angelopolotto@yahoo.com.br>
2019-03-19 12:11:26 -0300

    criação do carregamento de mensagens e upload de imagens do app

--- chat_online/lib/main.dart
+++ chat_online/lib/main.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() async {
   runApp(MyPage());
@@ -83,13 +87,27 @@ class _ChatScreenState extends State<ChatScreen> {
         body: Column(
           children: <Widget>[
             Expanded(
-              child: ListView(
-                children: <Widget>[
-                  ChatMessage(),
-                  ChatMessage(),
-                  ChatMessage(),
-                ],
-              ),
+              child: StreamBuilder(
+                  stream: Firestore.instance.collection("messages").snapshots(),
+                  builder: (context, snapshot) {
+                    switch(snapshot.connectionState) {
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        return Center (
+                          child: CircularProgressIndicator(),
+                        );
+                      default:
+                        return ListView.builder(
+                            reverse: true,
+                            itemCount: snapshot.data.documents.length,
+                            itemBuilder: (context, index) {
+                              List r = snapshot.data.documents.reversed.toList();
+                              return ChatMessage(r[index].data);
+                            }
+                        );
+                    }
+                  }
+              )
             ),
             Divider(
               height: 1.0,
@@ -139,7 +157,18 @@ class _TextComposerState extends State<TextComposer> {
           children: <Widget>[
             Container(
               child:
-                  IconButton(icon: Icon(Icons.photo_camera), onPressed: () {}),
+                  IconButton(icon: Icon(Icons.photo_camera), onPressed: () async {
+                    await _ensureLoggerIn();
+                    File imgFile = await ImagePicker.pickImage(source: ImageSource.camera);
+                    if (imgFile == null) return;
+                    StorageUploadTask task = FirebaseStorage.instance.ref().child(
+                        googleSignIn.currentUser.id.toString()
+                            + DateTime.now().millisecondsSinceEpoch.toString()
+                    ).putFile(imgFile);
+                    StorageTaskSnapshot taskSnapshot = await task.onComplete;
+                    String url = await taskSnapshot.ref.getDownloadURL();
+                    _sendMessage(imgUrl: url);
+                  }),
             ),
             Expanded(
                 child: TextField(
@@ -184,6 +213,10 @@ class _TextComposerState extends State<TextComposer> {
 }
 
 class ChatMessage extends StatelessWidget {
+  final Map<String, dynamic> data;
+
+  ChatMessage(this.data);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -194,8 +227,7 @@ class ChatMessage extends StatelessWidget {
           Container(
             margin: const EdgeInsets.only(right: 16.0),
             child: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  "https://designshack.net/wp-content/uploads/placehold.jpg"),
+              backgroundImage: NetworkImage(data["senderPhotoUrl"]),
             ),
           ),
           Expanded(
@@ -203,12 +235,14 @@ class ChatMessage extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(
-                "Daniel",
+                data["senderName"],
                 style: Theme.of(context).textTheme.subhead,
               ),
               Container(
                 margin: const EdgeInsets.only(top: 5.0),
-                child: Text("test"),
+                child: data["imgUrl"] != null
+                    ? Image.network(data["imgUrl"], width: 250.0)
+                    : Text(data["text"]),
               )
             ],
           ))
